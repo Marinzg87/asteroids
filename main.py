e233e02b52bf5b4057cead5fbad1bfd90cce7de3
Marcin Bogusz <bogusz.marcin@icloud.com>
2025-01-09 23:12:44 +0100

    main.py add screen

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import pygame
 from constants import * 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(F"Screen width: {SCREEN_WIDTH}")
     print(F"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black", rect=None)
+        pygame.display.flip()
+        # screen.flip()
+    
     
 if __name__ == "__main__":
     main()
