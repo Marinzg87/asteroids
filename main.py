b640722f8fcf2d799fe66aff18b7063ad867d29a
Marcin Bogusz <bogusz.marcin@icloud.com>
2025-01-10 20:29:22 +0100

    update player.py, main.py

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black", rect=None)
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    player.draw(screen)
+    
     
 if __name__ == "__main__":
     main()
